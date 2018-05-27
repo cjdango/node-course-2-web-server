f132d6790f2cfc006e801a1f0e943279ec6604f3
cjdango <cjdango@gmail.com>
2018-05-27 15:34:31 +0800

    added projects page

--- server.js
+++ server.js
@@ -46,6 +46,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Project Page',
+        welcomeMessage: 'Portfolio page'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Fucking Error'
